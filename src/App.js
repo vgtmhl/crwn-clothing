997a57e73f5240f0c45847d9ef8945791b18933c
vgtmhl <vgtmhl@gmail.com>
2020-04-21 11:28:50 +0200

    added sign in page and sign in component, added custom button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
