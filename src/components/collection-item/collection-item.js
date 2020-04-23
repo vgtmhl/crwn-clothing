e0a0246d8528177140b0bfba647845e96ce732d0
vgtmhl <vgtmhl@gmail.com>
2020-04-23 10:55:25 +0200

    updated shop data with new data and created cart utils with function to group items in cart reducer

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
-  console.log(imageUrl);
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
   return (
     <div className="collection-item">
-      <div className="image" style={{ background: `url(${imageUrl}) center center/cover` }} />
+      <div
+        className="image"
+        style={{ background: `url(${imageUrl}) center center/cover` }}
+      />
       <div className="collection-footer">
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  // true if the item already exists in the cart
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
